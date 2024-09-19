cbabfdf78a1e55c9ba832642239559800a2dedf0
Sasha Duke <sasha@zenrocklabs.io>
2024-09-19 13:03:20 +0100

    feat: implement `--output-file` flag to specify output path

--- utils/keygen/bls/main.go
+++ utils/keygen/bls/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
@@ -12,6 +14,7 @@ const keystorePath = "keystore/"
 
 func main() {
 	passwordFlag := flag.String("password", "", "Password for the keystore file")
+	outputFileFlag := flag.String("output-file", "", "Path to the output file including the filename")
 	flag.Parse()
 
 	keys, err := bls.GenRandomBlsKeys()
@@ -19,7 +22,19 @@ func main() {
 		panic(err)
 	}
 
-	filename := keystorePath + time.Now().Format("2006-01-02_15-04-05.000")
+	var filename string
+	if *outputFileFlag != "" {
+		filename = *outputFileFlag
+	} else {
+		filename = keystorePath + time.Now().Format("2006-01-02_15-04-05.000")
+	}
+
+	// Ensure the directory exists
+	outputDir := filepath.Dir(filename)
+	if err := os.MkdirAll(outputDir, 0700); err != nil {
+		panic(err)
+	}
+
 	if err = keys.SaveToFile(filename, *passwordFlag); err != nil {
 		panic(err)
 	}
--- utils/keygen/ecdsa/main.go
+++ utils/keygen/ecdsa/main.go
@@ -10,12 +10,14 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/google/uuid"
 )
 
 func main() {
 	passwordFlag := flag.String("password", "", "Password for the keystore file")
 	mnemonicFlag := flag.String("mnemonic", "", "Mnemonic phrase to derive the private key")
 	privateKeyFlag := flag.String("private-key", "", "Private key in hexadecimal format")
+	outputFileFlag := flag.String("output-file", "", "Path to the output file including the filename")
 	flag.Parse()
 
 	var privateKey *ecdsa.PrivateKey
@@ -34,28 +36,58 @@ func main() {
 		log.Fatalf("Failed to get or generate private key: %v", err)
 	}
 
-	// Create keystore directory
-	keystoreDir := "./keystore"
-	if err := os.MkdirAll(keystoreDir, 0700); err != nil {
-		log.Fatalf("Failed to create keystore directory: %v", err)
+	// Create Key object
+	key := &keystore.Key{
+		Id:         uuid.New(),
+		Address:    crypto.PubkeyToAddress(privateKey.PublicKey),
+		PrivateKey: privateKey,
 	}
 
-	// Create keystore
-	ks := keystore.NewKeyStore(keystoreDir, keystore.StandardScryptN, keystore.StandardScryptP)
+	// If output file is specified, store the key directly to that file
+	if *outputFileFlag != "" {
+		// Ensure the directory exists
+		outputDir := filepath.Dir(*outputFileFlag)
+		if err := os.MkdirAll(outputDir, 0700); err != nil {
+			log.Fatalf("Failed to create output directory: %v", err)
+		}
 
-	// Import the private key into the keystore
-	account, err := ks.ImportECDSA(privateKey, *passwordFlag)
-	if err != nil {
-		log.Fatalf("Failed to import private key: %v", err)
+		// Encrypt the key
+		keyJSON, err := keystore.EncryptKey(key, *passwordFlag, keystore.StandardScryptN, keystore.StandardScryptP)
+		if err != nil {
+			log.Fatalf("Failed to encrypt key: %v", err)
+		}
+
+		// Write the encrypted key to the specified file
+		err = os.WriteFile(*outputFileFlag, keyJSON, 0600)
+		if err != nil {
+			log.Fatalf("Failed to write keystore file: %v", err)
+		}
+
+		fmt.Printf("Keystore file created: %s\n", *outputFileFlag)
+	} else {
+		// Create keystore directory
+		keystoreDir := "./keystore"
+		if err := os.MkdirAll(keystoreDir, 0700); err != nil {
+			log.Fatalf("Failed to create keystore directory: %v", err)
+		}
+
+		// Create keystore
+		ks := keystore.NewKeyStore(keystoreDir, keystore.StandardScryptN, keystore.StandardScryptP)
+
+		// Import the private key into the keystore
+		account, err := ks.ImportECDSA(privateKey, *passwordFlag)
+		if err != nil {
+			log.Fatalf("Failed to import private key: %v", err)
+		}
+
+		// Get the absolute path of the keystore file
+		keystorePath, err := filepath.Abs(account.URL.Path)
+		if err != nil {
+			log.Fatalf("Failed to get absolute path: %v", err)
+		}
+
+		fmt.Printf("Keystore file created: %s\n", keystorePath)
 	}
-
-	// Get the absolute path of the keystore file
-	keystorePath, err := filepath.Abs(account.URL.Path)
-	if err != nil {
-		log.Fatalf("Failed to get absolute path: %v", err)
-	}
-
-	fmt.Printf("Keystore file created: %s\n", keystorePath)
 
 	// Print public address
 	address := crypto.PubkeyToAddress(privateKey.PublicKey)
